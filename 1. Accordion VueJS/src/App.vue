7e1ff6cd625bd4b577ac50ccaafc25b9b4d67ee1
Anna Fedoseeva <divoannet@gmail.com>
2019-07-17 09:39:35 +0700

    WIP

---
+++ 1. Accordion VueJS/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper" id="app">
+    <div class="block">
+      <h1>
+        Accordion demo
+      </h1>
+      <Accordion
+        :data="data"
+        :title="data.title"
+        :content="data.getContent"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue';
+
+const data = [
+  {
+    title: ''
+  }
+];
+
+export default {
+  name: 'app',
+  components: {
+    Accordion,
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  background: #1e2f40;
+  font-family: 'Noto Sans', Helvetica, Arial, sans-serif;
+}
+.wrapper {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.block {
+  background: #fff;
+  box-shadow: 0 0.2em 1em rgba(61,48,41,0.2);
+  > h1 {
+    padding: 24px 24px 16px;
+  }
+}
+</style>
---
+++ 1. Accordion VueJS/src/components/Accordion.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    Ololo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Accordion',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
